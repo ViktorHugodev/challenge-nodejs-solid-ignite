18888b9ee00469bc8a37e58de4539084b3f9bd97
ViktorHugoDev <viktorhugo.dev@gmail.com>
2022-08-23 16:15:44 -0300

    documentation using swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
 import { usersRoutes } from './routes/users.routes'
+import swaggerConfig from './swagger.json'
 
+// eslint-disable-next-line import/no-unresolved
 const app = express()
 
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use('/users', usersRoutes)
 
 export { app }
